b0fbdd5a8a4e5647034efa2ec2e42ea82177c9a0
Ricky-Kynd <Ricky-Kynd@gmail.com>
2016-04-20 21:26:55 +1200

    eloquent done

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,1 +1,45 @@
+/*
+  LOOPING A TRIANGLE
+*/
 
+for (var libraPondo = "#"; libraPondo.length <= 7; libraPondo += "#" )
+  console.log(libraPondo);
+
+/*
+  FIZZBUZZ
+*/
+
+var counter = "";
+
+for (var i = 1; i <= 100; i++) {
+	if (i % 3 === 0 && i % 5 === 0) {
+   		counter = "FizzBuzz";
+   } else if (i % 3 === 0) {
+   		counter = "Fizz";
+   } else if (i % 5 === 0) {
+   		counter = "Buzz";
+   } else {
+   		counter = i;
+   }
+  	console.log(counter);
+}
+
+
+/*
+  CHESS BOARD
+*/
+
+var size = 8;
+var kasparov = "";
+
+for (var i = 0; i < size; i++) {
+	for (var j = 0; j < size; j++) {
+    	if ((j + i) % 2 === 0) {
+        	kasparov += " ";
+        } else {
+        	kasparov += "#";
+        }
+    }       kasparov += "\n";
+}
+
+console.log(kasparov);
